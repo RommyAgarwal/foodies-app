4a1e809a16bea721f73002bee6970d05bdf34ed2
RommyAgarwal <rommyagarwal1@gmail.com>
2021-08-24 21:10:41 +0530

    commit repo1

--- public/js/login.js
+++ public/js/login.js
@@ -7,7 +7,6 @@ let forgetPassword = document.querySelector(".forgetPassword");
 
 forgetPassword.addEventListener("click" , async function(e){
     try{
-        // hiuhooijoo
         e.preventDefault();
         if(email.value){
             let obj = await axios.post("https://foodiesssss.herokuapp.com/api/user/forgetPassword" , {email:email.value});
