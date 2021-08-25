a848968418781e4297ac21fbbaf602eab4d5ce3a
RommyAgarwal <rommyagarwal1@gmail.com>
2021-08-25 19:38:50 +0530

    commit repo11

--- Controller/bookingController.js
+++ Controller/bookingController.js
@@ -61,7 +61,7 @@ async function checkoutComplete(req,res){
         const stripeSignature = req.headers['stripe-signature'];
         // console.log("payment done");
         // console.log( "req body=> " , req.body);
-        if(req.body.type == "checkout.session.object"){
+        if(req.body.type == "checkout.session.completed"){
             const userEmail = req.body.data.object.customer_email;
             const planId = req.body.data.object.client_reference_id;
             await createNewBooking(userEmail,planId);
